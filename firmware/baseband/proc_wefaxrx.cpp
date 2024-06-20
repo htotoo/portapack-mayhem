fc400d8c62011c18eda654b664d8b24756ae7c38
HTotoo <ttotoo@gmail.com>
2024-06-20 14:59:15 +0200

    test

--- firmware/baseband/proc_wefaxrx.cpp
+++ firmware/baseband/proc_wefaxrx.cpp
@@ -45,11 +45,11 @@ void WeFaxRx::update_params() {
             break;
     }
     // 840 px / line with line start
-    time_per_pixel = 60000000 / lpm * 840;                            // micros (595,2380952 at 120 lpm)
-    pxRem = (double)channel_filter_output_fs / ((double)lpm * 14.0);  // 840/60  = 228.57 sample / px
+    time_per_pixel = 60000000 / lpm * 840;                              // micros (595,2380952 at 120 lpm)
+    pxRem = (double)baseband_fs / 8.0 / 8.0 / 4.0 / ((int)lpm * 14.0);  // 840/60  = 228.57 sample / px
     samples_per_pixel = pxRem;
-    pxRem -= (double)samples_per_pixel;
-    pxRoll = pxRem;
+    pxRem -= samples_per_pixel;
+    pxRoll = 0;
 }
 
 double WeFaxRx::calculatePhaseAngle(int16_t i, int16_t q) {
@@ -68,48 +68,45 @@ double WeFaxRx::calculateFrequencyDeviation(complex16_t& iq, complex16_t& iqlast
         phaseDiff += 2.0 * M_PI;
     }
     // Calculate frequency deviation
-    return (phaseDiff / (2.0 * M_PI)) * channel_filter_output_fs;  // (sample rate)
+    return (phaseDiff / (2.0 * M_PI)) * 12000.0;  // (sample rate)
 }
 
 void WeFaxRx::execute(const buffer_c8_t& buffer) {
     // This is called at 3072000 / 2048 = 1500Hz
     if (!configured) return;
 
-    const auto decim_0_out = decim_0.execute(buffer, dst_buffer);              // /8 = 256
-    const auto decim_1_out = decim_1.execute(decim_0_out, dst_buffer);         // /8 = 32
-    const auto decim_2_out = decim_2.execute(decim_1_out, dst_buffer);         // /4 = 8
-    const auto channel_out = channel_filter.execute(decim_2_out, dst_buffer);  // /1 = 8
+    const auto decim_0_out = decim_0.execute(buffer, dst_buffer);       // /8 = 256
+    const auto decim_1_out = decim_1.execute(decim_0_out, dst_buffer);  // /8 = 32
+    const auto decim_2_out = decim_2.execute(decim_1_out, dst_buffer);  // /4 = 8
+    // const auto channel_out = channel_filter.execute(decim_2_out, dst_buffer);  // /1 = 8
 
-    feed_channel_stats(channel_out);
+    // feed_channel_stats(channel_out);
     // auto audio = demod.execute(channel_out, audio_buffer);
     // audio_output.write(audio);
 
     // todo process
-    for (size_t c = 0; c < channel_out.count; c++) {
+    for (size_t c = 0; c < decim_2_out.count; c++) {
         cnt++;
-        double freqq = calculateFrequencyDeviation(channel_out.p[c], iqlast);
-
+        double freqq = calculateFrequencyDeviation(decim_2_out.p[c], iqlast);
         if (status_message.freqmin > freqq) status_message.freqmin = freqq;
         if (status_message.freqmax < freqq) status_message.freqmax = freqq;
-        if (freqq > 4000 || freqq < 1000) continue;
         status_message.freqavg += (freqq - status_message.freqavg) / cnt;
-        iqlast = channel_out.p[c];
+        iqlast = decim_2_out.p[c];
         if (cnt >= (samples_per_pixel + (uint32_t)pxRoll)) {  // got a pixel
             cnt = 0;
             if (pxRoll >= 1) pxRoll -= 1;
             pxRoll += pxRem;
             status_message.freq = freqq;
             image_message.cnt++;  // saves the pixel
-            if (image_message.cnt < 380) {
-                // 2300 = white
-                // 1500 = black
-                if (status_message.freqavg >= 3400)
+            if (image_message.cnt < 480) {
+                image_message.image[image_message.cnt] = status_message.freqavg < 2500 ? 0 : 255;  // todo remove limit, send in multiple
+                /*if (status_message.freqavg >= 3000)
                     image_message.image[image_message.cnt] = 255;
-                else if (status_message.freqavg <= 1050)
+                else if (status_message.freqavg <= 2200)
                     image_message.image[image_message.cnt] = 0;
                 else {
-                    image_message.image[image_message.cnt] = 256 - (3400 - status_message.freqavg) / 3.15;
-                }
+                    image_message.image[image_message.cnt] = 256 - ((3000 - status_message.freqavg) / 3.1);
+                }*/
             }
             if (image_message.cnt >= 840) {
                 shared_memory.application_queue.push(image_message);
@@ -133,15 +130,19 @@ void WeFaxRx::on_message(const Message* const message) {
 }
 
 void WeFaxRx::configure(const WeFaxRxConfigureMessage& message) {
-    decim_0_input_fs = baseband_fs;
-    decim_0_output_fs = decim_0_input_fs / decim_0.decimation_factor;
-    decim_1_input_fs = decim_0_output_fs;
-    decim_1_output_fs = decim_1_input_fs / decim_1.decimation_factor;
-    decim_2_input_fs = decim_1_output_fs;
-    decim_2_output_fs = decim_2_input_fs / 4;
-    channel_filter_input_fs = decim_2_output_fs;
-    channel_filter_output_fs = channel_filter_input_fs / 1;  // 12000ul
     update_params();
+    constexpr size_t decim_0_input_fs = baseband_fs;
+    constexpr size_t decim_0_output_fs = decim_0_input_fs / decim_0.decimation_factor;
+
+    constexpr size_t decim_1_input_fs = decim_0_output_fs;
+    constexpr size_t decim_1_output_fs = decim_1_input_fs / decim_1.decimation_factor;
+
+    constexpr size_t decim_2_input_fs = decim_1_output_fs;
+    constexpr size_t decim_2_output_fs = decim_2_input_fs / 4;
+
+    constexpr size_t channel_filter_input_fs = decim_2_output_fs;
+    const size_t channel_filter_output_fs = channel_filter_input_fs / 1;  // 12000ul
+
     lpm = message.lpm;
     ioc_mode = message.ioc;
 
@@ -149,7 +150,7 @@ void WeFaxRx::configure(const WeFaxRxConfigureMessage& message) {
     decim_1.configure(taps_6k0_decim_1.taps);
     decim_2.configure(taps_6k0_decim_2.taps, 4);
     channel_filter.configure(taps_2k8_usb_channel.taps, 1);
-    demod.configure(channel_filter_output_fs, 2800);
+    demod.configure(channel_filter_output_fs, 3600);
     audio_output.configure(audio_24k_hpf_300hz_config, audio_24k_deemph_300_6_config, 0);
     configured = true;
 }
